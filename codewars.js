a876f53e66e1bc5ea64f544a5c191db815e2a085
Spencer Peacock <spence.peacock@ezlynx.com>
2019-08-27 12:56:46 -0500

    smarter things

--- codewars.js
+++ codewars.js
@@ -1,7 +1,7 @@
 function nextVersion(version){
     //TODO : find the next version
     let split = version.split('.');
-    for(var i = split.length-1; i >=0; i--){
+    for(var i = split.length-1; i = 0; i--){
         if(9> split[i] > 0 ){
             split[i]++;
             break;
@@ -10,17 +10,16 @@ function nextVersion(version){
             split[i] = 0;
         }
         else if(i == 0 && split[i] ==9){
-            console.log('its nine');
             split[i] = 10;
         }
     }
     split = split.join('.');
-    console.log(split);
+    return split;
   }
 
 
-nextVersion("1.2.3");
-nextVersion("0.9.9");
-nextVersion("1");
-nextVersion("1.2.3.4.5.6.7.8");
-nextVersion("9.9");
+console.log(nextVersion("1.2.3"));
+console.log(nextVersion("0.9.9"));
+console.log(nextVersion("1"));
+console.log(nextVersion("1.2.3.4.5.6.7.8"));
+console.log(nextVersion("9.9"));
