aac00bb5d616acea03d038f790ec2e3e7c543383
shpeacock <spencerpeacock@gmail.com>
2019-08-28 21:03:20 -0500

    playing around with some of the smarter solutions I found

--- phoneNum.js
+++ phoneNum.js
@@ -1,19 +1,30 @@
 function createPhoneNumber(numbers){
-    var formatPhone = [];
-    for(var i=0; i<numbers.length; i++){
-      if(i === 0){
-        formatPhone.push('(' + numbers[i]);
-      } else if(i === 2){
-        formatPhone.push(numbers[i] + ')');
-      } else if(i === 3){
-        formatPhone.push(' ' + numbers[i]);
-      } else if(i === 5){
-        formatPhone.push(numbers[i] + '-');
-      } else {
-        formatPhone.push(numbers[i]);
-      }
-    }
+    // var formatPhone = [];
+    // for(var i=0; i<numbers.length; i++){
+    //   if(i === 0){
+    //     formatPhone.push('(' + numbers[i]);
+    //   } else if(i === 2){
+    //     formatPhone.push(numbers[i] + ')');
+    //   } else if(i === 3){
+    //     formatPhone.push(' ' + numbers[i]);
+    //   } else if(i === 5){
+    //     formatPhone.push(numbers[i] + '-');
+    //   } else {
+    //     formatPhone.push(numbers[i]);
+    //   }
+    // }
     
-    return formatPhone.join('');
+    // return formatPhone.join('');
+
+    //other fun ways to solve 
+    //1
+        // numbers = numbers.join('');
+        // return '(' + numbers.substring(0, 3) + ') ' 
+        //     + numbers.substring(3, 6) 
+        //     + '-' 
+        //     + numbers.substring(6);
+
+    //smartest way I found 
+    return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/,'($1) $2-$3');
 }
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
