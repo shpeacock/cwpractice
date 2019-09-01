dc237970d0e1f1584a6c094c4c15c816fa4e8562
shpeacock <spencerpeacock@gmail.com>
2019-09-01 16:17:32 -0500

    more codewars practice

---
+++ dnaRna.js
@@ -0,0 +1,7 @@
+function DNAtoRNA(dna) {
+    // create a function which returns an RNA sequence from the given DNA sequence
+    return dna.replace(/T/g, "U");
+  }
+
+console.log(DNAtoRNA("TTT"));
+console.log(DNAtoRNA("GCAT"));
